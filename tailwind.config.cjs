744ee4a7f7dc58c2f852c56ecb5ca831f2ec7f64
j-avila <jose.leon.avila@gmail.com>
2023-09-05 13:09:12 -0300

    [WIP] building the grid

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        background: "#1F1F1F",
+        accent: "#50BF97",
+        text: "#828282",
+      },
+    },
+    plugins: [],
+  },
 }
