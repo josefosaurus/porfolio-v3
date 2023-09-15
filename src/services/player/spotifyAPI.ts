a0a2521ea62b2678dfa74753c9351b70cffbc875
j-avila <jose.leon.avila@gmail.com>
2023-09-14 23:27:34 -0300

    [wip] redirect

--- src/services/player/spotifyAPI.ts
+++ src/services/player/spotifyAPI.ts
@@ -12,8 +12,7 @@ export const getAuthorizationCode = (clientId: string) => {
   const redirectUri = `${window.location.href}`
   let lastSlashIndex = redirectUri.lastIndexOf("/");
   let updatedUrl = redirectUri.slice(0, lastSlashIndex) + redirectUri.slice(lastSlashIndex + 1);
-  const hardCodedURL = "https://joseavila.dev"
-  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${hardCodedURL}&scope=${scopes.join(
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri="https://joseavila.dev"&scope=${scopes.join(
     "%20"
   )}`
 
